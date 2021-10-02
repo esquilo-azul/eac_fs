d4e88d5194478208feaabf9144e68969de04018d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-01 22:47:32 -0300

    Object.fs_cache_object_id.

--- lib/eac_fs/patches/object/fs_cache.rb
+++ lib/eac_fs/patches/object/fs_cache.rb
@@ -5,6 +5,11 @@ require 'eac_fs/patches/module/fs_cache'
 class Object
   # @return [EacFs::Cache]
   def fs_cache
-    self.class.fs_cache
+    self.class.fs_cache.child(fs_cache_object_id)
+  end
+
+  # @return [String]
+  def fs_cache_object_id
+    raise 'Abstract method hit'
   end
 end
