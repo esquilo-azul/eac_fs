d586fcdced466bc3af0a6fb8bb6a718a62eb764a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-14 11:07:40 -0300

    Object.fs_cache_object_id: can return Array<String>.

--- lib/eac_fs/patches/object/fs_cache.rb
+++ lib/eac_fs/patches/object/fs_cache.rb
@@ -5,10 +5,12 @@ require 'eac_fs/patches/module/fs_cache'
 class Object
   # @return [EacFs::Cache]
   def fs_cache
-    self.class.fs_cache.child(fs_cache_object_id)
+    oid = fs_cache_object_id
+    oid = [oid.to_s] unless oid.is_a?(::Enumerable)
+    oid.inject(self.class.fs_cache) { |a, e| a.child(e.to_s) }
   end
 
-  # @return [String]
+  # @return [String, Array<String>]
   def fs_cache_object_id
     raise 'Abstract method hit'
   end
