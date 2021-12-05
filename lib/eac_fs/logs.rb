e705b5ecaf1b8cac58516916cbf5f6f533e9496a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-05 13:54:51 -0300

    EacFs::Logs::File: extract from EacFs::Logs.

--- lib/eac_fs/logs.rb
+++ lib/eac_fs/logs.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/string/filters'
+require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_utils/fs/temp'
 require 'filesize'
 
 module EacFs
-  module Logs
-    TRUNCATE_DEFAULT_LENGTH = 1000
-    TRUNCATE_APPEND_TEXT = '(...) '
+  class Logs
+    require_sub __FILE__
 
     # @param label [Symbol]
     # @return [EacRubyUtils::Fs::Temp::File]
     def [](label)
-      log_set.fetch(sanitize_label(label))
+      log_set.fetch(sanitize_label(label)).file
     end
 
     # @param label [Symbol]
     # @return [EacFs::Logs]
     def add(label)
-      log_set[sanitize_label(label)] = ::EacRubyUtils::Fs::Temp.file
+      file = ::EacFs::Logs::File.new(sanitize_label(label))
+      log_set[file.label] = file
 
       self
     end
@@ -36,27 +36,11 @@ module EacFs
       log_set.delete(sanitize_label(label))
     end
 
-    # @param label [Symbol]
     # @param length [Integer]
     # @return [String]
-    def truncate(label, length = TRUNCATE_DEFAULT_LENGTH)
-      content = self[label].read.strip
-      return content if content.length <= TRUNCATE_DEFAULT_LENGTH
-
-      TRUNCATE_APPEND_TEXT + content[content.length - length + TRUNCATE_APPEND_TEXT.length,
-        length - TRUNCATE_APPEND_TEXT.length]
-    end
-
-    # @param length [Integer]
-    # @return [String]
-    def truncate_all(length = TRUNCATE_DEFAULT_LENGTH)
-      s = "Files: #{log_set.length}\n"
-      log_set.each do |label, path|
-        x = truncate(label, length)
-        y = [label, path, ::Filesize.from("#{path.size} B").pretty].join(' / ')
-        s += x.blank? ? ">>> #{y} (Blank) <<<" : ">>> #{y}\n#{x}\n<<< #{y}\n"
-      end
-      s
+    def truncate_all(length = ::EacFs::Logs::File::TRUNCATE_DEFAULT_LENGTH)
+      "Files: #{log_set.length}\n" +
+        log_set.values.map { |file| file.truncate_with_label(length) }.join
     end
 
     private
@@ -67,7 +51,7 @@ module EacFs
       label.to_sym
     end
 
-    # @return [Hash<Symbol, EacRubyUtils::Fs::Temp::File>]
+    # @return [Hash<Symbol, EacFs::Logs::File>]
     def log_set
       @log_set ||= {}
     end
---
+++ lib/eac_fs/logs/file.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacFs
+  class Logs
+    class File
+      enable_simple_cache
+      common_constructor :label
+
+      TRUNCATE_DEFAULT_LENGTH = 1000
+      TRUNCATE_APPEND_TEXT = '(...) '
+
+      # @param length [Integer]
+      # @return [String]
+      def truncate(length = TRUNCATE_DEFAULT_LENGTH)
+        content = file.read.strip
+        return content if content.length <= TRUNCATE_DEFAULT_LENGTH
+
+        TRUNCATE_APPEND_TEXT + content[content.length - length + TRUNCATE_APPEND_TEXT.length,
+                                       length - TRUNCATE_APPEND_TEXT.length]
+      end
+
+      # @param length [Integer]
+      # @return [String]
+      def truncate_with_label(length = TRUNCATE_DEFAULT_LENGTH)
+        x = truncate(length)
+        y = [label, file, ::Filesize.from("#{file.size} B").pretty].join(' / ')
+        x.blank? ? ">>> #{y} (Blank) <<<" : ">>> #{y}\n#{x}\n<<< #{y}\n"
+      end
+
+      protected
+
+      # @return [EacRubyUtils::Fs::Temp::File
+      def file_uncached
+        ::EacRubyUtils::Fs::Temp.file
+      end
+    end
+  end
+end
