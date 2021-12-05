ba37cc63f1d41031914432cbee584a45259b5a55
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-05 13:54:28 -0300

    EacFs::Logs: document.

--- lib/eac_fs/logs.rb
+++ lib/eac_fs/logs.rb
@@ -9,27 +9,36 @@ module EacFs
     TRUNCATE_DEFAULT_LENGTH = 1000
     TRUNCATE_APPEND_TEXT = '(...) '
 
+    # @param label [Symbol]
+    # @return [EacRubyUtils::Fs::Temp::File]
     def [](label)
       log_set.fetch(sanitize_label(label))
     end
 
+    # @param label [Symbol]
+    # @return [EacFs::Logs]
     def add(label)
       log_set[sanitize_label(label)] = ::EacRubyUtils::Fs::Temp.file
 
       self
     end
 
+    # @return [EacFs::Logs]
     def remove_all
       log_set.each_key { |label| remove(label) }
 
       self
     end
 
+    # @param label [Symbol]
     def remove(label)
       log_set.fetch(sanitize_label(label)).remove
       log_set.delete(sanitize_label(label))
     end
 
+    # @param label [Symbol]
+    # @param length [Integer]
+    # @return [String]
     def truncate(label, length = TRUNCATE_DEFAULT_LENGTH)
       content = self[label].read.strip
       return content if content.length <= TRUNCATE_DEFAULT_LENGTH
@@ -38,6 +47,8 @@ module EacFs
         length - TRUNCATE_APPEND_TEXT.length]
     end
 
+    # @param length [Integer]
+    # @return [String]
     def truncate_all(length = TRUNCATE_DEFAULT_LENGTH)
       s = "Files: #{log_set.length}\n"
       log_set.each do |label, path|
@@ -50,10 +61,13 @@ module EacFs
 
     private
 
+    # @param label [Object]
+    # @return [Symbol]
     def sanitize_label(label)
       label.to_sym
     end
 
+    # @return [Hash<Symbol, EacRubyUtils::Fs::Temp::File>]
     def log_set
       @log_set ||= {}
     end
