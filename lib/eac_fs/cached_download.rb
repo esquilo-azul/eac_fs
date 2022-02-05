5d6c3774474c8e2e5f9d6546ef3827f2e3589790
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-05 00:06:49 -0300

    EacFs::CachedDownload: fix.

--- lib/eac_fs/cached_download.rb
+++ lib/eac_fs/cached_download.rb
@@ -3,13 +3,13 @@
 require 'eac_fs/patches'
 require 'eac_ruby_utils/fs/temp'
 
-module Avm
+module EacFs
   class CachedDownload
     attr_reader :url, :fs_cache
 
     def initialize(url, parent_fs_cache = nil)
       @url = url
-      @fs_cache = (parent_fs_cache || fs_cache).child(url.parameterize)
+      @fs_cache = (parent_fs_cache || fs_cache).child(url.to_s.parameterize)
     end
 
     def assert
