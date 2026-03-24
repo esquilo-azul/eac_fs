0c3dca0920c2ab0b89221b142f85d3bdb619c307
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-03-23 22:18:44 -0300

    eac_fs/core_ext.rb: remove.

--- lib/eac_fs.rb
+++ lib/eac_fs.rb
@@ -2,11 +2,10 @@
 
 require 'eac_ruby_utils'
 EacRubyUtils::RootModuleSetup.perform __FILE__ do
-  ignore 'core_ext'
   ignore 'patches'
 end
 
 module EacFs
 end
 
-require 'eac_fs/core_ext'
+require 'eac_fs/patches'
